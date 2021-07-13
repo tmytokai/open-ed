2179ebb51c562f90fdad24a6fed80064f840a643
tmytokai <tmytokai@yahoo.co.jp>
2021-07-13 13:39:33 +0900

    a-sin/exam/009/追加

--- docs/tex/makepdf-page.sh
+++ docs/tex/makepdf-page.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 TEXFILE=tmp/$2.tex
-DVIFILE=$2.dvi
-PDFFILE=$2.pdf
+#DVIFILE=$2.dvi
+#PDFFILE=$2.pdf
 
 mkdir -p tmp
 
-echo \\documentclass[a4paper]{jarticle}       >  ${TEXFILE}
+echo \\documentclass[a4paper]{jlreq}           >  ${TEXFILE}
 echo \\def\\rootdir{../../}                   >>  ${TEXFILE}
 echo \\def\\texdir{../../../../../../tex}     >>  ${TEXFILE}
-echo \\input{\\texdir/common-exam}            >>  ${TEXFILE}
+echo \\input{\\texdir/common-exam-lua}        >>  ${TEXFILE}
 echo \\def\\examname{過去問一覧}              >>  ${TEXFILE}
 echo \\def\\examdate{}                        >>  ${TEXFILE}
 
@@ -21,7 +21,7 @@ QFILE=tmp/${DIRPATH}.tex
 
 echo \\readinfo{tmp/${DIRPATH}}               >>  ${TEXFILE}
 
-echo \\def\\qpath{$1/$2/${DIRPATH}}        >  ${QFILE}
+echo \\def\\qpath{$1/$2/${DIRPATH}}            >  ${QFILE}
 echo \\def\\qpoint{10}                        >>  ${QFILE}
 echo \\def\\afile{a1}                         >>  ${QFILE}
 echo \\def\\bfile{a2}                         >>  ${QFILE}
@@ -37,8 +37,8 @@ echo %\\makeq[f]                             >>  ${TEXFILE}
 echo \\makeq[q]                              >>  ${TEXFILE}
 echo \\end{document}                         >>  ${TEXFILE}
 
-platex ${TEXFILE}
-dvipdfmx ${DVIFILE}
+lualatex ${TEXFILE}
+#dvipdfmx ${DVIFILE}
 
 rm -f *.aux *.log *.dvi *~
 rm -rf ./tmp
